d3d65c3e462f51cbc09f5b07cae03a79eabc6bb1
larikaroll <larikarol108@gmail.com>
2026-03-29 11:37:38 -0300

    feature/function-condicao-rank

--- index.js
+++ index.js
@@ -5,5 +5,21 @@ function getSaldoRank(getWin,getLoss){
     return getWin-getLoss
 }
 
+function condicaoRank(getWin){
+    if(getWin <= 10){
+        return "Ferro"
+    }else if(getWin > 10 && getWin <= 20 ){
+        return "Bronze"
+    }else if(getWin > 20 && getWin <= 50 ){
+        return "Prata"
+    }else if(getWin > 50 && getWin <= 80 ){
+        return "Ouro"
+    }else if(getWin > 80 && getWin <= 90 ){
+        return "Diamante"
+    }else if(getWin > 90 && getWin <= 100 ){
+        return "Lendário"
+    }else{
+        return "Imortal"
+    }
+}
 
-}while(contador >= 1)
